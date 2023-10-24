1c2c8a73e463b56f34348c95022b677075986e55
Abhishek Suryawanshi <iosdev.abhi@gmail.com>
2023-10-24 11:38:16 +0400

    SplashScreen/TabViewControllers

---
+++ RedDragon/RedDragon/AppModules/SplashScreen/SplashScreenVC.swift
@@ -0,0 +1,34 @@
+//
+//  SplashScreenVC.swift
+//  RedDragon
+//
+//  Created by QASR02 on 24/10/2023.
+//
+
+import UIKit
+
+class SplashScreenVC: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadFunctionality()
+    }
+    
+    func loadFunctionality() {
+        let textAnimator = ViewAnimator(anyView: titleLabel)
+        textAnimator.animate(initialScale: 0.001, finalScale: 2.5, duration: 0.4)
+        redirectToViewController()
+    }
+    
+    func redirectToViewController() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) { [self] in
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "TabView") as? UITabBarController
+            self.present(vc!, animated: true)
+        }
+    }
+
+
+}
