b79a06a08d1da2bdff194f136a4bb3cbd0d18520
Abhishek Suryawanshi <iosdev.abhi@gmail.com>
2023-10-24 12:11:41 +0400

    tabbar name

--- RedDragon/RedDragon/Constants/StringConstants.swift
+++ RedDragon/RedDragon/Constants/StringConstants.swift
@@ -9,6 +9,13 @@ import Foundation
 
 struct StringConstants {
     
+    //Tabbar names
+    static let home                 = "Home"
+    static let social               = "Social"
+    static let database             = "Database"
+    static let wallet               = "Wallet"
+    static let discover             = "Discover"
+    
     //SignIn screen
     static let signIn               = "Sign In"
     static let dontHaveAccount      = "Don't have an account?"
