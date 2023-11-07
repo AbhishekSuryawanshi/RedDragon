22b1db1ed8fdc4506ac6bde56eaf985bbbde3173
Abhishek Suryawanshi <iosdev.abhi@gmail.com>
2023-11-07 18:12:04 +0400

    code refactor

--- RedDragon/RedDragon/AppModules/MatchDetail/Controller/HighlightViewController.swift
+++ RedDragon/RedDragon/AppModules/MatchDetail/Controller/HighlightViewController.swift
@@ -23,7 +23,6 @@ class HighlightViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //symbolCollectionView.reloadData()
     }
     
     override func viewWillAppear(_ animated: Bool) {
