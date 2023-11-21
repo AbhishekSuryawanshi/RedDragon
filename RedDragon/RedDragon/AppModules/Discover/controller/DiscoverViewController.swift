291b61c69d2b7c2657d35d93f73f5c62866418c9
janainaIOS <janainamohanan1996@gmail.com>
2023-11-21 09:43:25 +0400

    Logout action added

--- RedDragon/RedDragon/AppModules/Discover/controller/DiscoverViewController.swift
+++ RedDragon/RedDragon/AppModules/Discover/controller/DiscoverViewController.swift
@@ -8,23 +8,30 @@
 import UIKit
 
 class DiscoverViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     
     // MARK: - Navigation
-
-     @IBAction func btnGoToBets(_ sender: Any) {
-         self.goToBets()
-     }
+    
+    @IBAction func btnGoToBets(_ sender: Any) {
+        self.goToBets()
+    }
     
     @IBAction func btnGoToMeet(_ sender: Any) {
         self.goToMeet()
+    }
+    
+    @IBAction func logoutButtonTapped(_ sender: UIButton) {
+        self.customAlertView_2Actions(title: "Logout".localized, description: StringConstants.logoutAlert.localized) {
+            UserDefaults.standard.user = nil
+            UserDefaults.standard.token = nil
+        }
     }
     
     func goToBets(){
@@ -34,5 +41,5 @@ class DiscoverViewController: UIViewController {
     func goToMeet(){
         navigateToViewController(MeetHomeVC.self, storyboardName: StoryboardName.meet, animationType: .autoReverse(presenting: .zoom))
     }
-     
+    
 }
