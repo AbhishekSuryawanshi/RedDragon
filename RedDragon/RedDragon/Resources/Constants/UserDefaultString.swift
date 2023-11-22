e1d3c0204b708a847bf5881903e3bc2d4f95f28a
Abhishek Suryawanshi <iosdev.abhi@gmail.com>
2023-11-22 18:19:45 +0400

    confict resolved

--- RedDragon/RedDragon/Resources/Constants/UserDefaultString.swift
+++ RedDragon/RedDragon/Resources/Constants/UserDefaultString.swift
@@ -21,10 +21,6 @@ struct UserDefaultString {
 
 struct DefaultToken {
   //  static let guestUser    = "1|EFDo4k2fYsjp3Lx676NK1z28CGLuCayNqDypLWXt"
-<<<<<<< HEAD
-    static let guestUser    = "112|SExzcUL2Sd08yEVfGraxFQizdzHvvxgHlu1Br3Gx5be467af"
-=======
     static let guestUser    = "101|XMCA1bzlO6Yxv8AKIVoqSJQO0EMPE09gyqwMHKOG9aea0678"
     static let session      = "17ba0791499db908433b80f37c5fbc89b870084b-eeb2319c2c71c4ad5636e0a27ae5a98852275a53"
->>>>>>> a6b2982 (PlaceBetDone)
 }
