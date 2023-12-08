d7fa659584f6c2f92496f4d660dce76b921dbbc3
Remya <ramyaissac.qooglobal@gmail.com>
2023-12-08 17:44:55 +0400

    Create team design started

---
+++ RedDragon/RedDragon/AppModules/StreetMatches/Controller/Teams/CreateStreetTeamVC.swift
@@ -0,0 +1,18 @@
+//
+//  CreateStreetTeamVC.swift
+//  RedDragon
+//
+//  Created by Remya on 12/8/23.
+//
+
+import UIKit
+
+class CreateStreetTeamVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+   
+}
