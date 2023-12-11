19d630475db17e2effc217c43b54f4ec36c0aa47
Abhishek Suryawanshi <iosdev.abhi@gmail.com>
2023-12-11 17:12:00 +0400

    Win/Loss VC

---
+++ RedDragon/RedDragon/AppModules/CardGame/Controller/Game/LossViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LossViewController.swift
+//  RedDragon
+//
+//  Created by QASR02 on 11/12/2023.
+//
+
+import UIKit
+
+class LossViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
