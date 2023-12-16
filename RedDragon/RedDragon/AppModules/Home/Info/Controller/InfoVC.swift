25219ab5f2e1b76e681cb8de38e1a7de752838f0
Abhishek Suryawanshi <iosdev.abhi@gmail.com>
2023-12-16 11:45:15 +0400

    Initial static data

--- RedDragon/RedDragon/AppModules/Home/Info/Controller/InfoVC.swift
+++ RedDragon/RedDragon/AppModules/Home/Info/Controller/InfoVC.swift
@@ -8,22 +8,48 @@
 import UIKit
 
 class InfoVC: UIViewController {
-
+    
+    @IBOutlet weak var firstStaticDataView: UIView!
+    @IBOutlet weak var predictionLabel: UILabel!
+    @IBOutlet weak var betLabel: UILabel!
+    @IBOutlet weak var socialLabel: UILabel!
+    @IBOutlet weak var fantasyLabel: UILabel!
+    @IBOutlet weak var quizLabel: UILabel!
+    @IBOutlet weak var matchesLabel: UILabel!
+    @IBOutlet weak var updatesLabel: UILabel!
+    @IBOutlet weak var databaseLabel: UILabel!
+    @IBOutlet weak var analysisLabel: UILabel!
+    @IBOutlet weak var usersLabel: UILabel!
+    @IBOutlet weak var meetAppLabel: UILabel!
+    @IBOutlet weak var streetMatchLabel: UILabel!
+    @IBOutlet weak var walletLabel: UILabel!
+    @IBOutlet weak var expertLabel: UILabel!
+    @IBOutlet weak var cardsLabel: UILabel!
+    
+    @IBOutlet weak var packageLabel: UILabel!
+    @IBOutlet weak var saveUptoLabel: UILabel!
+    @IBOutlet weak var packageSeeAllButton: UIButton!
+    @IBOutlet weak var firstPointsLabel: UILabel!
+    @IBOutlet weak var firstPriceLabel: UILabel!
+    @IBOutlet weak var secondPonitsLabel: UILabel!
+    @IBOutlet weak var secondPriceLabel: UILabel!
+    @IBOutlet weak var thirdPointsLabel: UILabel!
+    @IBOutlet weak var thirdPriceLabel: UILabel!
+    @IBOutlet weak var fourthPointsLabel: UILabel!
+    @IBOutlet weak var fourthPriceLabel: UILabel!
+    
+    @IBOutlet weak var bannerCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func appModulesButton(_ sender: UIButton) {
+        
     }
-    */
-
+    
+    @IBAction func packageSeeAllButton(_ sender: Any) {
+    }
+    
 }
---
+++ RedDragon/RedDragon/AppModules/Home/Info/View/BannerCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BannerCollectionViewCell.swift
+//  RedDragon
+//
+//  Created by QASR02 on 16/12/2023.
+//
+
+import UIKit
+
+class BannerCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var bannerImage: UIImageView!
+    
+}
