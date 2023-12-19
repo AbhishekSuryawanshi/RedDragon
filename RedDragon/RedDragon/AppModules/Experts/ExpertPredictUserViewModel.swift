0fa4650c6747b4ca29ac035adc9030e22c9dcf64
Abhishek Suryawanshi <iosdev.abhi@gmail.com>
2023-12-19 11:51:13 +0400

    modification for tags

--- RedDragon/RedDragon/AppModules/Experts/ExpertPredictUserViewModel.swift
+++ RedDragon/RedDragon/AppModules/Experts/ExpertPredictUserViewModel.swift
@@ -10,8 +10,19 @@ import Foundation
 class ExpertPredictUserViewModel: APIServiceManager<ExpertUserListModel> {
     
     ///function to fetch user list
-    func fetchExpertUserListAsyncCall(page: Int, slug: String) {
-        let urlString   = URLConstants.expertUserList + "?page=\(page)&app_slug=\(slug)"
+    func fetchExpertUserListAsyncCall(page: Int, slug: String, tag: String? = nil) {
+        var urlString   = URLConstants.expertUserList + "?page=\(page)&app_slug=\(slug)"
+        
+        if let tag = tag {
+            urlString += "&tag=\(tag)"
+        }
+        
+        let method      = RequestType.get
+        asyncCall(urlString: urlString, method: method, parameters: nil)
+    }
+    
+    func fetchExpertUserListAsyncCallWithTag(tag: String, page: Int, slug: String) {
+        let urlString   = URLConstants.expertUserList + "?tag=\(tag)&page=\(page)&app_slug=\(slug)"
         let method      = RequestType.get
         asyncCall(urlString: urlString, method: method, parameters: nil)
     }
