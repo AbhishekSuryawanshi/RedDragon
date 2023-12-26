931d5af78d8bdd8d5fbd60521550ce088a506431
Visakh Tharakan <visakhtQasrAlmas@gmail.com>
2023-12-26 11:49:24 +0400

    Cleaned code

--- RedDragon/RedDragon/Resources/Helper/Translation/Translation.swift
+++ RedDragon/RedDragon/Resources/Helper/Translation/Translation.swift
@@ -9,9 +9,9 @@ import UIKit
 
 final class Translation: APIServiceManager<TranslationModel> {
     static let shared = Translation()
-    let service: TranslationService
-    let request: TranslationRequest
-    var model: TranslationModel!
+    private let service: TranslationService
+    private let request: TranslationRequest
+    private var model: TranslationModel!
     
     init(
         request: TranslationRequest = TranslationRequest(),
@@ -54,19 +54,9 @@ extension Translation {
 
             case .failure(let error):
                 print("Translation data fetching failed.Reason:", error.description)
-                break
+                completion()
             }
         }
     }
-    
-//    private func saveTranslationsToCoreData(translations: [TranslationModel]) {
-//        // Implement Core Data save logic
-//    }
-//    
-//    private func fetchTranslationsFromCoreData() -> [TranslationModel] {
-//        // Implement Core Data fetch logic
-//        
-//        return []
-//    }
 
 }
