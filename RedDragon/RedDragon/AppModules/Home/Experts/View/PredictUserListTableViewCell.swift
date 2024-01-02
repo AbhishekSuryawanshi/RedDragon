64badb2defa3df54850f4ae23a8351b4fb3b6c86
Shailja Bharti <shailjabharti02@gmail.com>
2024-01-02 16:25:25 +0400

    Issue fixed

--- RedDragon/RedDragon/AppModules/Home/Experts/View/PredictUserListTableViewCell.swift
+++ RedDragon/RedDragon/AppModules/Home/Experts/View/PredictUserListTableViewCell.swift
@@ -14,6 +14,7 @@ class PredictUserListTableViewCell: UITableViewCell {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var aboutLabel: UILabel!
     @IBOutlet weak var winRateLabel: UILabel!
+    @IBOutlet weak var winRateTitleLabel: UILabel!
     @IBOutlet weak var allCountLabel: UILabel!
     @IBOutlet weak var successCountLabel: UILabel!
     @IBOutlet weak var unsuccessCountLabel: UILabel!
